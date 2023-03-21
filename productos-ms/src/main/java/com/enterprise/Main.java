d497934e861164f1ae78a2ea313a85a19fa2cc2d
osolarte <oms.omar@gmail.com>
2023-03-20 19:51:55 -0500

    changing to dynamic config

--- productos-ms/src/main/java/com/enterprise/Main.java
+++ productos-ms/src/main/java/com/enterprise/Main.java
@@ -1,8 +1,10 @@
 package com.enterprise;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableAutoConfiguration
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
